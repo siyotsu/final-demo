a2fe75e47cfdcf27dfa45cc84788033ede03405c
teslaluv <109180406+teslaluv@users.noreply.github.com>
2022-09-26 00:44:55 +0300

    Update output.tf

--- modules/ecr/output.tf
+++ modules/ecr/output.tf
@@ -2,7 +2,7 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.ecr_repository.repository_url
 }
 
-output "reg_id" {
+output "registry_id" {
   value = aws_ecr_repository.ecr_repository.registry_id
 }
 
