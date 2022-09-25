f959ce119f7829f7f21da9ce556198d6e1c0715a
teslaluv <109180406+teslaluv@users.noreply.github.com>
2022-09-26 01:29:15 +0300

    Update variables.tf

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -29,6 +29,18 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
+variable "public_subnet_cidrs" {
+  type = list
+    # default = [
+    # "10.0.1.0/24",
+    # "10.0.2.0/24"
+    # ]
+}
+
+variable "private_subnet_cidrs" {
+  type = list
+}
+
 # variable "env" {
 #   type = string
 #   # default = "dev"
