d51c30838d7efca3682cea0aa20a7e2ebac5e9c6
teslaluv <109180406+teslaluv@users.noreply.github.com>
2022-09-29 19:42:05 +0300

    Update main.tf

--- modules/init-build/main.tf
+++ modules/init-build/main.tf
@@ -2,12 +2,11 @@
 resource "null_resource" "build" {
   provisioner "local-exec" {
     command = "make build"
-    # working_dir = "/home/teslaluv/demo3/project/app"
     working_dir = var.working_dir
     environment = {
         TAG = var.image_tag
-        REGISTRY_ID = var.reg_id
-        REPOSITORY_REGION = var.region
+        REGISTRY_ID = var.registry_id
+        REPOSITORY_REGION = var.aws_region
         APP_NAME = var.app_name
     }
   }
